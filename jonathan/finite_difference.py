646597eb1b1704ae08d105c7981ffccc834938dc
Jonathan Bollig <jonathan.bollig@uni-jena.de>
2025-11-04 18:32:13 +0100

    Implemented nd version

--- jonathan/finite_difference.py
+++ jonathan/finite_difference.py
@@ -2,49 +2,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
-class GridND:   # depricated
+class GridND:
     """
     Started with an n-dimensional class, and then decided, this is over engineered and a waste of time. 
     Maybe I will continue here in the future :)
     """
-    def __init__(self, shape=(10, 10), dimensions=((0, 1), (0, 1)), n_ghost=0):
-        self.shape = shape
+    def __init__(self, shape=(10, 10), dimensions=((0, 1), (0, 1)), n_ghost=0, f=None):
+        if len(shape) != len(dimensions):
+            raise ValueError(f"Length of shape and dimensions have to be the same, but where {len(self.shape)} and {len(self.dimensions)}")
+        if f is not None and (np.array(f.shape) != np.array(shape)+2*n_ghost).all():
+            raise ValueError(f"The shape of f does not agree with shape: {np.array(f.shape)} vs {np.array(shape)+2*n_ghost}") 
+        self.shape = shape  # length of every dimension without ghost! 
         self.n_ghost = n_ghost
         self.dimensions = dimensions
-        if len(self.shape) != len(self.dimensions):
-            raise ValueError(f"Length of shape and dimensions have to be the same, but where {len(self.shape)} and {len(self.dimensions)}")
-        self.grid = self.create_grid()
+        self.hs = np.array([(dim[1] - dim[0])/len_ for len_, dim in zip(shape, dimensions)])
+        self.vectors = [self.create_vector(dim, h, n_ghost, len_) for dim, h, len_ in zip(dimensions, self.hs, shape)]
+        self.f = f 
+    
+    @staticmethod
+    def create_vector(dim, h, n_ghost, len_):
+        return np.linspace(dim[0] + (-n_ghost+1/2)*h, dim[1] + (n_ghost-1/2)*h, len_ + 2*n_ghost)
 
-    def create_grid(self):
-        vectors = []  # list that contains the vector of every dimension
-        for length, dimension in zip(self.shape, self.dimensions):
-            x = np.linspace(dimension[0], dimension[1], length)
-            vectors.append(x)
-        return np.meshgrid(*vectors)
+    def calc_f(self, func):
+        # maybe "func" should be saved as well? 
+        # this uses broadcasting, see https://www.geeksforgeeks.org/numpy/numpy-array-broadcasting/
+        # self.f = func(*(self.arrs[i][(slice(None) if dim == i else None) for dim in range(n)] for i in range(n)))
+        broadcasted_views = np.ix_(*self.vectors)
+        self.f = func(*broadcasted_views)
 
-    def finite_difference(self, method="center", implementation="numpy"):
-        pass
+    def imshow(self):
+        if len(self.f.shape) != 2:
+            raise ValueError(f"Can only plot functions with 2 dimensions, but f has {len(self.f.shape)} dimensions.")
+        fig, ax = plt.subplots()
+        ax.imshow(self.f)
+        plt.show()
+
+    def center_difference(self):
+        # returns n new instances of a nd grid class, containing gradients as f
+        # np.gradient explenation: https://numpy.org/doc/stable/reference/generated/numpy.gradient.html
+        gradients = np.gradient(self.f, *self.hs)
+        return [GridND(self.shape, self.dimensions, self.n_ghost, gradient) for gradient in gradients]
 
 class Grid2D:
-    def __init__(self, x_n=10, y_n=10, x_dim=(0, 1), y_dim=(0, 1), n_ghost=2):
+    def __init__(self, x_n=10, y_n=10, x_dim=(0, 1), y_dim=(0, 1), n_ghost=2, f=None):
         self.x_n, self.y_n = x_n, y_n
         self.n_ghost = n_ghost
         self.x_dim, self.y_dim = x_dim, y_dim
         self.dx, self.dy = (x_dim[1] - x_dim[0])/x_n, (y_dim[1] - y_dim[0])/y_n 
-        self.xs, self.ys = self.create_linspace(x_dim, self.dx, n_ghost, x_n), self.create_linspace(y_dim, self.dy, n_ghost, y_n)
-        self.f = None
+        self.xs, self.ys = self.create_vector(x_dim, self.dx, n_ghost, x_n), self.create_vector(y_dim, self.dy, n_ghost, y_n)
+        self.f = f  # could add sanity check if shape of f matches x_n and y_n.
 
     @staticmethod
-    def create_linspace(x_dim, dx, n_ghost, x_n):
-        return np.linspace(x_dim[0] - n_ghost*dx + 1/2*dx, x_dim[1] + n_ghost*dx - 1/2*dx, x_n + 2*n_ghost)
+    def create_vector(dim, h, n_ghost, len_):
+        return np.linspace(dim[0] + (-n_ghost+1/2)*h, dim[1] + (n_ghost-1/2)*h, len_ + 2*n_ghost)
     
     def calc_f(self, func):
         # maybe "func" should be saved as well? 
         # this uses broadcasting, see https://www.geeksforgeeks.org/numpy/numpy-array-broadcasting/
         self.f = func(self.xs[:, None], self.ys[None, :])
-
-    def set_f(self, f):
-        self.f = f
 
     def imshow(self):
         fig, ax = plt.subplots()
@@ -79,23 +94,30 @@ class Grid2D:
             y_grad = np.concatenate(([top], y_grad.T, [bottom])).T  # a little hacky with the two '.T', but I think it's correct
         else:
             raise ValueError(f"Method {method} is not supported for 'center difference'")
-        x_grad_grid = Grid2D(self.x_n, self.y_n, self.x_dim, self.y_dim, self.n_ghost)
-        x_grad_grid.set_f(x_grad)
-        y_grad_grid = Grid2D(self.x_n, self.y_n, self.x_dim, self.y_dim, self.n_ghost)
-        y_grad_grid.set_f(y_grad)
+        x_grad_grid = Grid2D(self.x_n, self.y_n, self.x_dim, self.y_dim, self.n_ghost, x_grad)
+        y_grad_grid = Grid2D(self.x_n, self.y_n, self.x_dim, self.y_dim, self.n_ghost, y_grad)
         return x_grad_grid, y_grad_grid
 
 if __name__ == "__main__":
-    my_grid = Grid2D(10, 10, (0, np.pi), (0, np.pi), 2)
+    my_grid = Grid2D(10, 20, (0, np.pi), (0, 2*np.pi), 2)
     my_grid.calc_f(lambda x, y: np.sin(x)*y)
     my_grid.imshow()
     d_dx, d_dy = my_grid.center_difference(method="numpy")
     d_dx2, d_dy2 = my_grid.center_difference(method="manual")
-    
+
     # check if both methods yield the same result
     print(np.max(np.abs(d_dx.f-d_dx2.f)))
     print(np.max(np.abs(d_dy.f-d_dy2.f)))
-
+  
     # plot the gradients
     d_dx.imshow()
     d_dy.imshow()
+
+    # ND Version
+    my_grid = GridND((10, 20), ((0, np.pi), (0, 2*np.pi)), 2)
+    my_grid.calc_f(lambda x, y: np.sin(x)*y)
+    my_grid.imshow()
+
+    gradients = my_grid.center_difference()
+    for gradient in gradients:
+        gradient.imshow()
