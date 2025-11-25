d64cdcbd5470d4aae7c6114bd4b3132fc6f88819
Jonathan Bollig <jonathan.bollig@uni-jena.de>
2025-11-25 15:49:38 +0100

    improved 1D plotting function

--- common/grid1D.py
+++ common/grid1D.py
@@ -4,30 +4,33 @@ import matplotlib.pyplot as plt
 class Grid1D:
     def __init__(self, x_n=10, x_dim=(0, 1), n_ghost=2, f=None):
         self.x_n = x_n
-        self.n_ghost = n_ghost
         self.x_dim = x_dim
+        self.n_ghost = n_ghost
         self.dx = (x_dim[1] - x_dim[0])/x_n 
-        self.xs = self.create_vector(x_dim, self.dx, n_ghost, x_n)
-        self.f = f  # could add sanity check if shape of f matches x_n and y_n.
+        self.xs = np.linspace(x_dim[0] + (-n_ghost+1/2)*self.dx, x_dim[1] + (n_ghost-1/2)*self.dx, x_n + 2*n_ghost)
+        self.f = f  # could add sanity check if shape of f matches x_n 
 
-    @staticmethod
-    def create_vector(dim, h, n_ghost, len_):
-        return np.linspace(dim[0] + (-n_ghost+1/2)*h, dim[1] + (n_ghost-1/2)*h, len_ + 2*n_ghost)
-    
     def calc_f(self, func):
         # maybe "func" should be saved as well? 
         self.f = func(self.xs)
 
-    def imshow(self):
+    def imshow(self, marker="x", linestyle="", extra_grids = []):
         fig, ax = plt.subplots()
-        ax.plot(self.xs, self.f)
+        ax.plot(self.xs, self.f, marker=marker, linestyle=linestyle)
+        for extra_grid in extra_grids:
+            ax.plot(extra_grid.xs, extra_grid.f, marker=marker, linestyle=linestyle)
+        ax.axvline(self.x_dim[0], c="black", linestyle=":")
+        ax.axvline(self.x_dim[1], c="black", linestyle=":")
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.grid()
         plt.show()
 
-    def center_difference(self, method="numpy"):
+    def center_difference(self, method="manual"):
         """
-        returns 2 new instances of a 2d grid class, containing x and y gradient as f
+        returns a new instances of a 1d grid class, containing the gradient as f
         allowed methods:
-        - numpy  # uses numpy gradient --> center differential
+        - numpy  --> uses numpy gradient --> center differential
         - manual 
         """
         if method == "numpy":
@@ -44,13 +47,16 @@ class Grid1D:
 
 if __name__ == "__main__":
     my_grid = Grid1D(20, (0, 2*np.pi), 2)
-    my_grid.calc_f(lambda x: np.sin(x))
+    my_grid.calc_f(np.sin)
     my_grid.imshow()
     d_dx = my_grid.center_difference(method="numpy")
     d_dx2 = my_grid.center_difference(method="manual")
 
     # check if both methods yield the same result
     print(np.max(np.abs(d_dx.f-d_dx2.f)))
-  
-    # plot the gradients
+
+    # plot the gradient
     d_dx.imshow()
+
+    # plot both together
+    my_grid.imshow(extra_grids=[d_dx])
